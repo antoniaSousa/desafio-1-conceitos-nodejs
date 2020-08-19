001703f545c3aaf816df9d5446737487e6baba1f
antonia <antonia@unifesspa.edu.br>
2020-08-18 23:40:25 -0300

    Desafio conceitos nodejs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+ const {uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,73 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId(request, response, next){
+  const {id} = request.params;
+
+  if(isUuid(id)){
+    return response.status(400).json({error: "Invalid repository ID"});
+
+  }
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  const {title} = request.query;
+
+  const result = title
+  ? repositories.filter(repository => repository.title.includes(title))
+  : repositories;
+  return response.json(result);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs, likes} = request.body;
+  const repository = {id: uuid(), title, url, techs, likes};
+  repositories.push(repository);
+  return response.json(repository)
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+app.put("/repositories/:id", validateRepositoryId,(request, response) => {
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id ==id);
+   if(repositoryIndex < 0){
+     return response.status(400).json({error: 'repositori not foun'})
+   }
+   const repository = {
+     id,
+     title,
+     url,
+     techs
+    
+   };
+   repositories[repositoryIndex] = repository;
+   return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id",validateRepositoryId, (request, response) => {
+
+  const {id} = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id ==id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: 'Repository not found'})
+  }
+  repositories.splice(repositoryIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.query;
+ 
+  const repositoryIndex = repositories.findIndex(repository=> repository.id == id);
+   if(!repositoryIndex){
+      return response.status(400).json({erro:"Repository not found"});
+   }
+   repository[repositoryIndex].like++;
+   return response.json(repository);
 });
 
 module.exports = app;
